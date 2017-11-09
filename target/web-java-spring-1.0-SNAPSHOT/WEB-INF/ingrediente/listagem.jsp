b52be06b69c538c2c34e24e8a512656ded38e156
dotorida <dotoridas@gmail.com>
2017-11-09 21:42:38 +0000

    added ,gitignore

---
+++ target/web-java-spring-1.0-SNAPSHOT/WEB-INF/ingrediente/listagem.jsp
@@ -0,0 +1,45 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<DOCTYPE html>
+<html>
+<head>
+    <title>${titulo}</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <c:set var="path" value="${pageContext.request.contextPath}" scope="request"/>
+		<title>${titulo} - ${pageContext.request.contextPath}</title>
+        
+        <style type="text/css">
+            @IMPORT url("${path}/static/bootstrap/css/bootstrap.min.css");
+            @IMPORT url("${path}/static/bootstrap/css/bootstrap-theme.min.css");
+        </style>
+               
+</head>
+<body>
+    
+
+    <c:if test="${not empty mensagemErro}">
+        <div class="container">
+            <div class="alert alert-danger">${mensagemErro}</div>
+        </div>
+    </c:if>
+    
+    <c:if test="${not empty mensagemInfo}">
+        <div class="container">
+            <div class="alert alert-info">${mensagemInfo}</div>
+        </div>
+    </c:if>
+    
+     <section class="container" id="secao-ingrediente">
+        <jsp:include page="tabela-ingrediente.jsp" />
+    </setion>
+    
+            <jsp:include page="modal-ingrediente.jsp"/>
+    
+            <script type="text/javascript" src="${path}/static/js/jquery-2.1.3.min.js"></script>
+            <script type="text/javascript" src="${path}/static/bootstrap/js/bootstrap.min.js"></script>
+            <script type="text/javascript" src="${path}/static/js/ingredientes.js"></script>
+</body>
+</html>
---
+++ target/web-java-spring-1.0-SNAPSHOT/static/js/pizzas.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+   
+   aplicarListeners();
+   
+   aplicarListenerBtnSalvar();
+   
+});
+
+ 
+ var limparDados = function(){
+      $('#id').val('');
+      $('#nome').val('');
+      $('#preco').val('');
+      $('#categoria').val('');
+      $('#ingredientes option').attr('selected', false);
+     
+ }; 
+ 
+ 
+var aplicarListenerBtnSalvar = function(){
+    $('#btn-salvar').click(function(){
+        var url='pizzas';
+        var dadosPizza =  $('#form-pizza').serialize(); 
+        $.post(url,dadosPizza)
+          .done(function(pagina) {
+             $('#secao-pizza').html(pagina);
+             aplicarListeners();
+          })
+          .fail(function() {
+            alert( 'Erro ao salvar');
+          })
+          .always(function(){
+                $('#modal-ingrediente').modal('hide');
+            });
+              
+    }); 
+};
+
+var aplicarListeners = function(){
+    
+  $('#modal-ingrediente').on('hide.bs.modal', limparDados);
+   
+   
+   $('.btn-editar').click(function(){
+      
+       var idPizza = $(this).parents('tr').data('id');
+       $.ajax({
+          url:'pizzas/'+idPizza,
+          type:'GET',
+          success: function(pizza){
+              $('#id').val(pizza.id);
+              $('#nome').val(pizza.nome);
+              $('#preco').val(pizza.preco);
+              $('#categoria').val(pizza.categoria);
+              $('#ingredientes').val(pizza.ingredientes);
+              
+              pizza.ingredientes.forEach(function(ingrediente){
+                  var id = ingrediente.id;
+                  $('#ingredientes option[value='+id+']').attr('selected',true);
+              })
+              
+              $('#modal-pizza').modal('show');
+                        
+              }
+          
+          
+          
+       });
+       
+    });
+    
+    
+  
+    
+    $('.btn-deletar').click(function(){
+        var id = $(this).parents('tr').data('id');
+        
+        $.ajax({
+            url: 'pizzas/'+id,
+            type: 'DELETE',
+            success: function(result) {
+                 $('tr[data-id="'+id+'"]').remove(); 
+                 
+                 var pizzas= parseInt($('#quantidade-pizzas').text());
+                 $('#quantidade-pizzas').text(pizzas-1);
+            }
+        });
+    });
+    
+    
+    
+};
