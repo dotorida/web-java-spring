9c56144763c45597347c6378eb62b3d0f369bcb9
dotorida <dotoridas@gmail.com>
2017-11-10 01:19:01 +0000

    reviwed a ingrediente.js

--- src/main/webapp/static/js/ingredientes.js
+++ src/main/webapp/static/js/ingredientes.js
@@ -1,79 +1,148 @@
 $(document).ready(function(){
+	
+	aplicarListeners();
+	
+	aplicatListenerBtnSalvar();
+	
+});
 
-   aplicarListeners();
+var aplicatListenerBtnSalvar = function(){
+	$('#btn-salvar').on('click', function(){
+		var url = 'ingredientes';
+		var dadosIngrediente = $('#form-ingrediente').serialize();
+		
+		$.post(url, dadosIngrediente)
+			.done(function(pagina){
+				$('#secao-ingredientes').html(pagina)
+				aplicarListeners();
+				
+			})
+			.fail(function(){
+				alert('Erro ao salvar!');
+				
+			})
+			.always(function(){
+				$('#modal-ingrediente').modal('hide');
+			});
+	});
+}
+
+
+var limparModal = function(){
+	$('#id').val('');
+	$('#nome').val('');
+	$('#categoria').val('');
+};
+
+
+var aplicarListeners = function(){
+	$('#modal-ingrediente').on('hide.bs.modal', limparModal);
+	
+	$('.btn-editar').on('click', function(){
+		var id = $(this).parents('tr').data('id');
+		var url = 'ingredientes/'+id;
+		
+		$.get(url)
+			.success(function(ingrediente){
+				$('#id').val(ingrediente.id);
+				$('#nome').val(ingrediente.nome);
+				$('#categoria').val(ingrediente.categoria);
+				
+				$('#modal-ingrediente').modal('show');
+			});
+	});
+	
+	
+	$('.btn-deletar').on('click', function(){
+		var id = $(this).parents('tr').data('id');
+		var csrf = $('#csrf').val();
+		
+		$.ajax({
+			url : "ingredientes/"+id,
+			type: 'DELETE',
+			headers: {'X-CSRF-TOKEN': csrf},
+		    success: function(result) {
+		    	$('tr[data-id="'+id+'"]').remove();
+				var ingredientes = parseInt( $('#quantidade-ingredientes').text() );
+		    	$('#quantidade-ingredientes').text(ingredientes - 1);
+		    }
+		});
+		
+		
+	});
+	
+}/*$(document).ready(function(){
+
+  aplicarListeners();
     
-      aplicarListenerBtnSalvar();
+    aplicarListenerBtnSalvar();
   
- })
+ });
  
  var aplicarListenerBtnSalvar = function(){
-     
-     $('#btn-salvar').click(function(){
-        
+     $('#btn-salvar').on('click', function(){        
         var url='ingredientes';
         var dadosIngrediente =  $('#form-ingrediente').serialize(); 
-       // console.log(dadosIngrediente);
+        
         $.post(url,dadosIngrediente)
           .done(function(pagina) {
-             $('#secao-ingrediente').html(pagina);
+             $('#secao-ingrediente').html(pagina)
              aplicarListeners();
-          })
+         
+         })
           .fail(function() {
             alert( 'Erro ao salvar');
           })
+          
           .always(function(){
                $('#modal-ingrediente').modal('hide');
-            })
+            });
     });
  }
  
- var limparDados = function(){
+ var limparModal = function(){
        $('#id').val('');
        $('#nome').val('');
        $('#categoria').val('');
  }
  
  
+ 
     var aplicarListeners = function(){
+     $('#modal-ingrediente').on('hide.bs.modal', limparModal);
       
-     $('#modal-ingrediente').on('hide.bs.modal', limparDados);
-      
-      $('.btn-editar').click(function(){
+      $('.btn-editar').on('click',function(){
          var id = $(this).parents('tr').data('id'); 
          var url ='ingredientes/'+id;
-         
+          
          $.get(url)
-            .success(function(ingrediente){
-                
+            .success(function(ingrediente){      
                 $('#id').val(ingrediente.id);
                 $('#nome').val(ingrediente.nome);
                 $('#categoria').val(ingrediente.categoria);
             
                 $('#modal-ingrediente').modal('show');
-            });
-         
+            }); 
       });
       
-      
-    
-    $('.btn-deletar').click(function(){
-       
+         
+    $('.btn-deletar').on('click',function(){    
        var id = $(this).parents('tr').data('id'); 
        var ingredientes = $('#quantidade-ingredientes').text();
        
        $.ajax({
                 url: 'ingredientes/'+id,
                 type: 'DELETE',
+                headers: {'X-CSRF-TOKEN': csrf},
                 success: function(result) {
-                  $('tr[data-id="'+id+'"]').remove();     
+                  $('tr[data-id="'+id+'"]').remove();  
+                  var ingredientes = parseInt( $('#quantidade-ingredientes').text() );
                    $('#quantidade-ingredientes').text(ingredientes-1);
                 }
         });
-    })
- }
-  
-  
 
-   
-   
-  
+
+
+    })
+ 
+}*/
